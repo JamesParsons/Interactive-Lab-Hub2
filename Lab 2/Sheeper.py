199f52fb0298e918777574054d5f8f7f077d134c
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-09-23 06:29:59 -0700

    Add files via upload

--- Lab 2/Sheeper.py
+++ Lab 2/Sheeper.py
@@ -4,7 +4,6 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
-from tkinter import *
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -37,27 +36,8 @@ width = disp.height
 image = Image.new("RGB", (width, height))
 rotation = 90
 
-## make the root window
-#root = Tk()
-#root.state('zoomed')
-
-##getting screen width and height of display
-#screenwidth= root.winfo_screenwidth() 
-#screenheight= root.winfo_screenheight()
-
-#print(screenwidth, screenheight)
-
-#newwidth = screenwidth
-#newheight = screenheight
-
-#canvas = Canvas(root, width=newwidth, height=newheight)
-#canvas.pack()
-
-#width = newwidth
-#height = newheight
-
 # Get drawing object to draw on image.
-#draw = ImageDraw.Draw(image)
+draw = ImageDraw.Draw(image)
 
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
@@ -70,10 +50,10 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-#Alternatively load a TTF font.  Make sure the .ttf font file is in the
-#same directory as the python script!
-#Some other nice fonts to try: http://www.dafont.com/bitmap.php
-##font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
@@ -113,10 +93,30 @@ while True:
     ghue = mintime * 4    
     bhue = hrtime * 10
     
+    # face
+    draw.ellipse((center-60,center-60,center+60,center+60),outline=(rhue,ghue,bhue), fill=(None))
     
-    #sky
-    #canvas.create_rectangle(0,0,newwidth,newheight,fill="#00CDFF")
-    #canvas.update()    
+    if sectime == 0:
+        #draw.rectangle((0,0,width,height),fill=(0,0,0))
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    
+    if sectime >= 10 and sectime <= 59: # left eye
+        draw.ellipse((center-40,center-40,center-10,center-10),outline=(rhue,ghue,bhue), fill=(None))
+    if sectime >= 20 and sectime <= 59: # right eye
+        draw.ellipse((center+10,center-40,center+40,center-10),outline=(rhue,ghue,bhue), fill=(None))
+    if sectime >= 30 and sectime <= 59: #left ball
+        draw.ellipse((center-32,center-24,center-18,center-10),outline=(rhue,ghue,bhue), fill=(0,0,bhue))
+    if sectime >= 40 and sectime <= 59: # right ball
+        draw.ellipse((center+18,center-24,center+32,center-10),outline=(rhue,ghue,bhue), fill=(0,0,bhue))
+    if sectime >= 50 and sectime <= 59: # lips
+        #draw.arc((center-40,center+10,center+40,center+40),start=1,end=-1,fill=(rhue,0,0),width=3)
+        #draw.arc((center-40,center+10,center+40,center+40),start=.5,end=-.5,fill=(rhue,0,0),width=3)
+        draw.ellipse((center-40,center+10,center+40,center+40),outline=(rhue,ghue,bhue),fill=(rhue,0,0),width=3)
+    # show colors of sec, min, hr on side
+    draw.rectangle((center + 70, 0, width, h1), fill=(rhue,0,0))
+    draw.rectangle((center + 70, h1, width, h2), fill=(0,ghue,0))
+    draw.rectangle((center + 70, h2, width, height), fill=(0,0,bhue))
+    
     
     
     # Display image.

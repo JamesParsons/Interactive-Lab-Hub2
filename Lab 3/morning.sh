4aa9cb6002dc327d48071761114a4f4a2776a63f
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-09-30 10:52:19 -0400

    Add files via upload

--- Lab 3/morning.sh
+++ Lab 3/morning.sh
@@ -4,4 +4,3 @@
 say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 #say $*
 say " Good morning Say ler.  It is ok to call you James"
- 

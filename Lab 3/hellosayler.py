3ac97eee42a41202e150254f03052027dced83e3
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-01 11:02:47 -0400

    Add files via upload

--- Lab 3/hellosayler.py
+++ Lab 3/hellosayler.py
@@ -2,5 +2,21 @@
 import pyttsx3
 
 engine = pyttsx3.init()
+
+
+
+
+# RATE
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 80)   # slower is lower number
+
+# VOICE
+voices = engine.getPRoperty('voices')
+#engine.setProperty('voice', voices[0].id) #male
+engine.setProperty('voice', voices[1].id) #female
+
+
+
+
 engine.say("Hello say ler.  it is ok if people call you james")
 engine.runAndWait()

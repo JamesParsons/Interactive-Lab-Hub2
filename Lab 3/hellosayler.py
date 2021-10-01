e06bc1899e8b20e793a9e721dbbfaaf71de41dd7
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-01 13:02:15 -0400

    Add files via upload

--- Lab 3/hellosayler.py
+++ Lab 3/hellosayler.py
@@ -13,7 +13,8 @@ engine.setProperty('rate', 125)   # slower is lower number
 # VOICE
 voices = engine.getProperty('voices')
 #engine.setProperty('voice', voices[11].id) #male
-engine.setProperty('voice', 'english_rp+f3')
+#engine.setProperty('voice', 'english_rp+f3') # high female
+engine.setProperty('voice', 'english_rp+f4')
 #engine.setProperty('voice', voices[1].id) #male australian?
 
 #x = 0
@@ -30,5 +31,5 @@ engine.say("Hello say ler.  it is ok if people call you james")
 engine.runAndWait()
 
 
-
+# female voice found here https://stackoverflow.com/questions/57751564/pyttsx3-voice-gender-female
 # gotten from https://pyttsx3.readthedocs.io/en/latest/engine.html#module-pyttsx3.voice

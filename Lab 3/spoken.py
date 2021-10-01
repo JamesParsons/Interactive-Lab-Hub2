905e3045a24272a60a091c0061a2f2f4c06f1453
JamesParsons <jsp285@cornell.edu>
2021-10-01 16:43:15 -0400

    commit message

---
+++ Lab 3/spoken.py
@@ -0,0 +1,15 @@
+
+from gtts import gTTS
+#from playsound import playsound
+import os
+
+#Message = "Hello"
+#speech = gTTS(text="Howdy")
+#speech.save('testsound.mp3')
+#playsound('testsound.mp3')
+
+text = "Hello!"
+tts = gTTS(text, lang='en')
+tts.save("hi.mp3")
+
+os.system("hi.mp3")
---
+++ Lab 3/welcome.sh
@@ -0,0 +1,5 @@
+
+
+
+# from https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+espeak -ven+f2 -k5 -s150 --stdout  "Welcome say ler.  It is ok if everyone calls you James" | aplay

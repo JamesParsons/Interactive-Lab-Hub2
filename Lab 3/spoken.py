4f6d6b202300478bab8672aa7569780ff9de569a
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-01 11:41:05 -0400

    Add files via upload

--- Lab 3/spoken.py
+++ Lab 3/spoken.py
@@ -1,10 +1,15 @@
 
 from gtts import gTTS
 from playsound import playsound
+import os
 
-Message = "Hello"
-speech = gTTS(text="Howdy")
-speech.save('testsound.mp3')
-playsound('testsound.mp3')
+#Message = "Hello"
+#speech = gTTS(text="Howdy")
+#speech.save('testsound.mp3')
+#playsound('testsound.mp3')
 
+text = "Hello!"
+tts = gTTS(text)
+tts.save("hi.mp3")
 
+os.system("hi.mp3")

2eff2a1a1c8da2157ec2915f3efc3f060f9cf8d7
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-01 11:13:43 -0400

    Add files via upload

--- Lab 3/hellosayler.py
+++ Lab 3/hellosayler.py
@@ -12,14 +12,14 @@ engine.setProperty('rate', 125)   # slower is lower number
 
 # VOICE
 voices = engine.getProperty('voices')
-#engine.setProperty('voice', voices[0].id) #male
+engine.setProperty('voice', voices[0].id) #male
 #engine.setProperty('voice', voices[1].id) #male australian?
 
-x = 0
-for voice in voices:
-    engine.setProperty('voice', voice.id)
-    engine.say('this voice number is' + str(x))
-    x = x + 1
+#x = 0
+#for voice in voices:
+    #engine.setProperty('voice', voice.id)
+    #engine.say('this voice number is' + str(x))
+    #x = x + 1
 
 
 engine.say("Hello say ler.  it is ok if people call you james")
